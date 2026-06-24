19036d84f7680cfff421b87829b7b2fc51b92b8b
Hu man <romaincontant.80@gmail.com>
2026-06-24 08:52:46 +0100

    Update assets.js

--- js/assets.js
+++ js/assets.js
@@ -123,3 +123,4 @@ assetsManager.load('Elf_west', 'assets/skins/Elf_west.png');
 assetsManager.load('Elf_est', 'assets/skins/Elf_est.png');
 assetsManager.load('Elf_back', 'assets/skins/Elf_back.png');
 assetsManager.load('Elf_front', '/Elf_front.png');
+assetsManager.load('stairs_down', 'assets/decors/stairs_down.png'); //A FAIRE!!
